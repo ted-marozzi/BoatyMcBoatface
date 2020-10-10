223506f7c5f8dff75caf3621a5a1ca5c640f81d5
bixin-li <bixinl@student.unimelb.edu.au>
2020-10-10 21:38:58 +1100

    Basic player controls and camera tracking
    will add hp bar and smoother rotation in later updates

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,48 @@
+﻿// Script for Straight-shooting cannonball, no gravity
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CannonBall : MonoBehaviour
+{
+    public GameObject createOnDestroy;
+    public Rigidbody rb;
+    public float velocity;
+    public string tagToDamage;
+    public bool type1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (type1)
+        {
+            //this.transform.Translate(velocity * Time.deltaTime);
+            rb.position += transform.forward * Time.deltaTime * velocity;
+
+        }
+    }
+
+    // Handle collisions
+    void OnTriggerEnter(Collider col)
+    {
+        // Destroy self
+        Destroy(this.gameObject);
+        GameObject obj = Instantiate(this.createOnDestroy);
+        obj.transform.position = this.transform.position;
+
+        // Destroy Object
+        /*
+        if (col.gameObject.tag == tagToDamage)
+        {
+            Destroy(col.gameObject);
+        }
+        */
+    }
+}
