76b2dd2f0d06511e951df7042fc7c31287dad21d
bixin-li <bixinl@student.unimelb.edu.au>
2020-10-19 16:03:41 +1100

    Merged type4 controller & removed unused scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float yawSpeed = 15f;
+    public float pitchSpeed = 15f;
+
+    Rigidbody rb;
+    float[] yawLimit = { -90f, 90f };
+    float[] pitchLimit = { -60f, 60f };
+    float pitch = 0.0f;
+    float yaw = 0.0f;
+    float initialPitch = 32.5f; //65 degree around x-axis
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pitch = initialPitch;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pitch -= Input.GetAxis("Mouse Y");
+        yaw += Input.GetAxis("Mouse X");
+
+        rb.transform.localEulerAngles = new Vector3(pitch * pitchSpeed, yaw * yawSpeed, 0f);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,29 @@
-﻿// COMP30019 Graphics and Interaction Project 1, September 2020
-// Controller for "flight-simulator" style camera and terrain re-generation
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Type3Controller : MonoBehaviour
+public class PlayerController: MonoBehaviour
 {
     // Speed of Movement
-    public float moveSpeed = 15;
-    public float rotateSpeed = 1;
+    public float moveSpeed = 15f;
+    public float rotateSpeed = 15f;
     public float[] cannonOffset = { 0.0f, 1.0f, 5.8f };
     public GameObject cannonTemplate;
 
     Rigidbody rb;
-    float yaw = 0.0f;
+    float rotate;
+
+    // Key Assignment
+    KeyCode forwardKey = KeyCode.W;
+    KeyCode leftKey = KeyCode.A;
+    KeyCode rightKey = KeyCode.D;
+    KeyCode fireKey = KeyCode.Space;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rotate = 0f;
     }
 
 
@@ -28,21 +32,20 @@ public class Type3Controller : MonoBehaviour
 
         // Camera Movement
         float df = 0.0f, r = 0.0f;
-        if (Input.GetKey(KeyCode.W)) { df += 1.0f; }
-        if (Input.GetKey(KeyCode.A)) { r -= 1.0f; }
-        if (Input.GetKey(KeyCode.D)) { r += 1.0f; }
-        yaw += r;
+        if (Input.GetKey(forwardKey)) { df += 1.0f; }
+        if (Input.GetKey(leftKey)) { r -= 1.0f; }
+        if (Input.GetKey(rightKey)) { r += 1.0f; }
 
         rb.position += transform.forward * df * Time.deltaTime * moveSpeed;
-        rb.transform.localEulerAngles = new Vector3(0f, yaw, 0f) * rotateSpeed;
+        rotate += r;
+        rb.transform.localEulerAngles = new Vector3(0f, rotate, 0f) * rotateSpeed;
 
         //Fire
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(fireKey))
         {
             GameObject cannonball = GameObject.Instantiate<GameObject>(cannonTemplate);
             cannonball.transform.localRotation = this.transform.localRotation;
             cannonball.transform.localPosition = this.transform.localPosition + transform.right * cannonOffset[0] + transform.forward * cannonOffset[2] + new Vector3(0.0f, cannonOffset[1], 0.0f);
         }
-
     }
 }
