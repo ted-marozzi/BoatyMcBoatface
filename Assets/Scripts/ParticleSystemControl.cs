609dc21fe30bd1315757e95ee8f2e5f911802412
bixin-li <bixinl@student.unimelb.edu.au>
2020-10-22 02:25:29 +1100

    Fix particle system
    fixed the problem that particle system continue to exist after ends

---
+++ Assets/Scripts/ParticleSystemControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemControl : MonoBehaviour
+{
+    public float lifeTime;
+
+    void Update()
+    {
+        // Make particle system self-destruct after playing
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
