adf05b945f24f97d00b91a8731ebad3d45ad9294
bixin-li <bixinl@student.unimelb.edu.au>
2020-11-01 14:38:02 +1100

    Simple start menu

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,24 @@
+﻿// This script is for all button controls (switch scene, open menu, etc.)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour
+{
+    string levelOne = "Level1";
+    string levelTwo = "Level2";
+    string levelThree = "Level3";
+    string MainMenu = "MainMenu";
+
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene(levelOne);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
