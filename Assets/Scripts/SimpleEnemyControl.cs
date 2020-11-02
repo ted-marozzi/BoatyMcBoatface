fa2c16379060ce81955c988113b53fd7cd8194b6
Edward Marozzi <38032037+ted-marozzi@users.noreply.github.com>
2020-11-02 21:14:56 +1100

    Enhanced enemy ai

--- Assets/Scripts/SimpleEnemyControl.cs
+++ Assets/Scripts/SimpleEnemyControl.cs
@@ -8,37 +8,57 @@ public class SimpleEnemyControl : MonoBehaviour
 {
 
     public KeyCode fireKey;
-    public bool chasePlayer;
-    public float speed = 0.0f;
+    private bool chasePlayer = true;
+    private float speed = 10.0f;
     public GameObject player;
-
+    private float timer = 0f;
     Rigidbody rb;
     CannonControl cannon;
+    public int seed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        Random.InitState(seed);
+        
         rb = GetComponent<Rigidbody>();
         cannon = GetComponent<CannonControl>();
+        timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer+= Time.deltaTime;
         float step = speed * Time.deltaTime;
+        keepFlat();
         // Moves toward player
         if (chasePlayer)
         {
             transform.LookAt(player.transform);
-            rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
+            transform.position += transform.forward * Time.deltaTime * speed;
+            
         }
-
+        float var = Random.Range(1,4);
         // Fire Cannon
-        if (Input.GetKeyDown(fireKey))
+        if (timer>5.0f + var)
         {
-            cannon.fireCannon();
+            timer = 0;
+            //cannon.fireCannon();
         }
     }
+    
+    void keepFlat() {
+        
+        Vector3 tmp = transform.localEulerAngles;
+
+        tmp.x = 0;
+        tmp.z = 0;
+
+        transform.localEulerAngles = tmp;
+
+    }
+
 }
 
 
