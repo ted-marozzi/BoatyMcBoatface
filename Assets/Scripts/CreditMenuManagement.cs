744dd223a3fe018d3027df2924f7d878315edf9a
bixin-li <bixinl@student.unimelb.edu.au>
2020-11-03 21:59:34 +1100

    Advanced credit menu

--- Assets/Scripts/CreditMenuManagement.cs
+++ Assets/Scripts/CreditMenuManagement.cs
@@ -9,29 +9,33 @@ public class CreditMenuManagement : MonoBehaviour
     public GameObject creditsPage2;
     public GameObject creditsPage3;
     public GameObject creditsPage4;
+    public GameObject creditsPage5;
 
+    // Page Management
     public void ShowPage1()
     {
         closeAllPages();
         creditsPage1.SetActive(true);
     }
-
     public void ShowPage2()
     {
         closeAllPages();
         creditsPage2.SetActive(true);
     }
-
     public void ShowPage3()
     {
         closeAllPages();
         creditsPage3.SetActive(true);
     }
-
     public void ShowPage4()
     {
         closeAllPages();
         creditsPage4.SetActive(true);
+    }
+    public void ShowPage5()
+    {
+        closeAllPages();
+        creditsPage5.SetActive(true);
     }
 
     void closeAllPages()
@@ -40,5 +44,6 @@ public class CreditMenuManagement : MonoBehaviour
         creditsPage2.SetActive(false);
         creditsPage3.SetActive(false);
         creditsPage4.SetActive(false);
+        creditsPage5.SetActive(false);
     }
 }
