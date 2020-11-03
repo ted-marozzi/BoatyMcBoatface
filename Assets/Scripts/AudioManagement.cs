807db198ca669d4de30b9d8b036c2762a0c5e40b
bixin-li <bixinl@student.unimelb.edu.au>
2020-11-03 19:32:15 +1100

    Add music to lv1

---
+++ Assets/Scripts/AudioManagement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagement : MonoBehaviour
+{
+    public AudioSource Music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Play Background Music
+        Music = this.GetComponent<AudioSource>();
+        Music.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
